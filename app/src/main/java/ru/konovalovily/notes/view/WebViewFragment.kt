c9756c99bf3766988b980065d1e3484bbceccb1d
ZeroNineDev <zeronine.development@gmail.com>
2022-01-20 08:50:19 +0400

    19.01 Добавил отображение html в WebView

--- app/src/main/java/ru/konovalovily/notes/view/WebViewFragment.kt
+++ app/src/main/java/ru/konovalovily/notes/view/WebViewFragment.kt
@@ -1,23 +1,44 @@
 package ru.konovalovily.notes.view
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import ru.konovalovily.notes.R
+import android.webkit.WebViewClient
+import ru.konovalovily.notes.databinding.FragmentWebViewBinding
 
 class WebViewFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentWebViewBinding? = null
+    private val binding: FragmentWebViewBinding
+        get() = _binding ?: throw RuntimeException("FragmentWebViewBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_web_view, container, false)
+    ): View = FragmentWebViewBinding.inflate(inflater).also {
+        _binding = it
+    }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupWebView()
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun setupWebView() {
+        binding.webViewHolder.apply {
+            loadUrl("https://github.com/KonovalovIly")
+            settings.javaScriptEnabled = true
+        }
+        binding.webViewHolder.webViewClient = WebViewClient()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
